25fe6d32ad67c2a123d846d371d97828cb297398
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-04-26 11:06:21 +0200

    Added trending movie list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Trending } from './Trending';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Trending />
     </div>
   );
 };
--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from 'react';
+import api from '../services/api';
+
 export const Trending = () => {
-  return <></>;
+  const [movies, setMovies] = useState([]);
+  const [apiError, setApiError] = useState(null);
+
+  useEffect(() => {
+    const fetchTrendingMovies = async () => {
+      try {
+        const response = await api.fetchTrendingMovies();
+        setMovies(response.data.results);
+        console.log(response.data.results);
+      } catch (error) {
+        setApiError(error);
+      }
+    };
+
+    fetchTrendingMovies();
+  }, []);
+
+  return (
+    <>
+      <h1>Trending today:</h1>
+      <ul>
+        {movies.map(movie => (
+          <li>
+            <a href="#">{movie.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
