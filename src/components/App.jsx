86fe7b905a5dd57d7260cc8260b39c4354940a7a
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-04-27 22:40:26 +0200

    Added SearchParams to Search component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,27 +4,10 @@ import { Search } from '../pages/Search';
 import { Trending } from '../pages/Trending';
 import { MovieDetails } from '../pages/MovieDetails';
 import { NotFound } from '../pages/NotFound.jsx';
-import { useState } from 'react';
-import api from '../services/api';
 import Cast from './Cast';
 import Reviews from './Reviews';
 
 export const App = () => {
-  const [movies, setMovies] = useState();
-
-  const handleSearch = async event => {
-    event.preventDefault();
-    const searchQuery = event.target.query.value;
-
-    try {
-      const response = await api.fetchMoviesWithQuery(searchQuery);
-      // console.log(response.data);
-      setMovies(response.data.results);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <div
       style={{
@@ -38,10 +21,7 @@ export const App = () => {
       <Navigation />
       <Routes>
         <Route path="/" element={<Trending />} />
-        <Route
-          path="/movies"
-          element={<Search onSearch={handleSearch} movieList={movies} />}
-        />
+        <Route path="/movies" element={<Search />} />
         <Route path="/movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,26 +1,56 @@
 import { MoviesList } from 'components/MoviesList';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import api from '../services/api';
 
 export const Search = props => {
+  const [movies, setMovies] = useState();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') ?? '';
+
   const renderMovieList = movies => {
     if (movies != null) {
       return <MoviesList moviesList={movies} />;
     }
   };
 
+  useEffect(() => {
+    if (query === '') return;
+    const fetchMoviesWithQuery = async event => {
+      try {
+        const response = await api.fetchMoviesWithQuery(query);
+        // console.log(response.data);
+        setMovies(response.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchMoviesWithQuery(query);
+  }, [query]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    setSearchParams({ query: form.elements.query.value });
+  };
+
   return (
     <div>
-      <form className="search-form" onSubmit={props.onSearch}>
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           className="search-form__input"
           type="text"
           name="query"
           placeholder="Search movies"
+          value={query}
+          onChange={event => setSearchParams({ query: event.target.value })}
         />
         <button className="search-form__button" type="submit">
           Search
         </button>
       </form>
-      {renderMovieList(props.movieList)}
+      {renderMovieList(movies)}
     </div>
   );
 };
